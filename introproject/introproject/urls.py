9dff8b35b3f11d187ab68541f535455764adf7ff
yangsubinn <cindy010670@gmail.com>
2021-07-23 22:47:43 +0900

    photo 페이지 Create 구현

--- introproject/introproject/urls.py
+++ introproject/introproject/urls.py
@@ -1,3 +1,4 @@
+# from introproject.subin.views import write
 from django.contrib import admin
 from django.urls import path
 from subin import views
@@ -10,5 +11,7 @@ urlpatterns = [
     path('music/', views.music, name="music"),
     path('photo/', views.photo, name="photo"),
     path('<str:id>', views.detail, name="detail"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
     path('main/', views.main, name="main"),
 ]
--- introproject/subin/views.py
+++ introproject/subin/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Photo
+from django.utils import timezone
 
 def home(request):
     return render(request, "home.html")
@@ -20,6 +21,18 @@ def photo(request):
 def detail(request, id):
     photo = get_object_or_404(Photo, pk=id)
     return render(request, "detail.html", {'photo':photo})
-    
+
+def new(request):
+    return render(request, "new.html")
+
+def create(request):
+    new_photo = Photo()
+    new_photo.title = request.POST['title']
+    new_photo.writer = request.POST['writer']
+    new_photo.body = request.POST['body']
+    new_photo.pub_date = timezone.now()
+    new_photo.save()
+    return redirect('detail', new_photo.id)
+
 def main(request):
     return render(request, "main.html")
